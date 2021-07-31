26bad8f17dcec2304b7b4cce9a68a85c1e26330f
yealynn-kim <yealynn.kim@navercorp.com>
2021-07-31 22:35:27 +0900

    add multiple share element usage

--- app/src/main/java/study/android/bodyprofile/MainActivity.kt
+++ app/src/main/java/study/android/bodyprofile/MainActivity.kt
@@ -1,23 +1,36 @@
 package study.android.bodyprofile
 
+import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.core.app.ActivityOptionsCompat
 
+import android.util.Pair as UtilPair
+
 class MainActivity : AppCompatActivity() {
-    val imageView: ImageView by lazy { findViewById<ImageView>(R.id.main_image_view) }
+    val mainImageView: ImageView by lazy { findViewById<ImageView>(R.id.main_image_view) }
+    val subImageView: ImageView by lazy { findViewById<ImageView>(R.id.sub_image_view) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        imageView.setOnClickListener {
-            val intent = Intent(this, TransitionTestActivity::class.java)
+        val intent = Intent(this, TransitionTestActivity::class.java)
+        mainImageView.setOnClickListener {
+            val options = ActivityOptions.makeSceneTransitionAnimation(
+                this,
+                UtilPair.create(mainImageView, mainImageView.transitionName),
+                UtilPair.create(subImageView, subImageView.transitionName)
+            )
+            startActivity(intent, options.toBundle())
+        }
+        subImageView.setOnClickListener {
             val option = ActivityOptionsCompat.makeSceneTransitionAnimation(this, it, it.transitionName)
 
             startActivity(intent, option.toBundle())
         }
     }
+
 }
