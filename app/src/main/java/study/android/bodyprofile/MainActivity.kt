f0c6ce27532b9976d693674e4cdfc8ebc7e8dd90
yealynn-kim <yealynn.kim@navercorp.com>
2021-07-31 22:24:38 +0900

    test shared element

--- app/src/main/java/study/android/bodyprofile/MainActivity.kt
+++ app/src/main/java/study/android/bodyprofile/MainActivity.kt
@@ -1,11 +1,23 @@
 package study.android.bodyprofile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.app.ActivityOptionsCompat
 
 class MainActivity : AppCompatActivity() {
+    val imageView: ImageView by lazy { findViewById<ImageView>(R.id.main_image_view) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView.setOnClickListener {
+            val intent = Intent(this, TransitionTestActivity::class.java)
+            val option = ActivityOptionsCompat.makeSceneTransitionAnimation(this, it, it.transitionName)
+
+            startActivity(intent, option.toBundle())
+        }
     }
 }
